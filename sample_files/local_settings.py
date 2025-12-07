b0a99de89f64d3f9300f97198ccfd46812b7bf23
Leonides <53244679+leo20826@users.noreply.github.com>
2025-12-07 12:08:34 -0500

    Update local_settings.py

--- sample_files/local_settings.py
+++ sample_files/local_settings.py
@@ -17,7 +17,7 @@ DEBUG = True  # Change to False once you are done with runserver testing.
 
 # Uncomment and set to the domain names this site is intended to serve.
 # You must do this once you set DEBUG to False.
-#ALLOWED_HOSTS = ['oj.vnoi.info']
+#ALLOWED_HOSTS = ['localhost']
 
 # Optional apps that DMOJ can make use of.
 INSTALLED_APPS += (
@@ -53,8 +53,8 @@ DATABASES = {
 
 # Internationalization.
 # Documentation: <https://docs.djangoproject.com/en/3.2/topics/i18n/>
-LANGUAGE_CODE = 'vi'
-DEFAULT_USER_TIME_ZONE = 'Asia/Ho_Chi_Minh'
+LANGUAGE_CODE = 'en'
+DEFAULT_USER_TIME_ZONE = 'America/Havana'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -110,7 +110,7 @@ ADMINS = (
 )
 
 # The sender for the aforementioned emails.
-SERVER_EMAIL = 'VNOJ: VNOI Online Judge <vnoj@vnoi.info>'
+SERVER_EMAIL = 'OJ: Online Judge <oj@oj.com>'
 
 
 ################################################
@@ -136,11 +136,11 @@ STATIC_ROOT = '/tmp/static'
 ############################################
 
 ## DMOJ site display settings.
-SITE_NAME = 'VNOJ'
-SITE_FULL_URL = 'https://oj.vnoi.info'
-SITE_LONG_NAME = 'VNOJ: VNOI Online Judge'
+SITE_NAME = 'OJ'
+SITE_FULL_URL = 'https://localhost'
+SITE_LONG_NAME = 'OJ: Online Judge'
 SITE_ADMIN_EMAIL = 'admin@example.com'
-TERMS_OF_SERVICE_URL = '//oj.vnoi.info/tos/'  # Use a flatpage.
+TERMS_OF_SERVICE_URL = '//localhost/tos/'  # Use a flatpage.
 
 ## Media files settings.
 # This is the directory where all the media files are stored.
